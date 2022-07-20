5496872db3f758469dcbf6b38941607c26d7ae45
rodrigo <rodrigosluz@gmail.com>
2022-07-20 11:43:37 -0300

    Isolamento de imports

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,14 +1,28 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('rluz10')
+    assert avatar_url == url
+
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/103536637?v=4'
     resp_mock.json.return_value = {
         'login': 'Rluz10', 'id': 103536637, 'node_id': 'U_kgDOBivX_Q',
-        'avatar_url': 'https://avatars.githubusercontent.com/u/103536637?v=4'
+        'avatar_url': url
     }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
-    url = github_api.buscar_avatar('rluz10')
-    assert 'https://avatars.githubusercontent.com/u/103536637?v=4' == url
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('renzo')
+    assert 'https://avatars.githubusercontent.com/u/402714?v=4' == url
